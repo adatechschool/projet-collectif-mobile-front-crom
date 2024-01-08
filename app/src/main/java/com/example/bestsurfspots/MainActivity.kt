18d16d2eac37581fe16a8cf756a870f9a239b2c9
aanatema <romane.boireau@gmail.com>
2024-01-08 16:05:19 +0100

    tentatiive d'ajout de liste

--- app/src/main/java/com/example/bestsurfspots/MainActivity.kt
+++ app/src/main/java/com/example/bestsurfspots/MainActivity.kt
@@ -1,38 +1,25 @@
 package com.example.bestsurfspots
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+
 import android.view.Menu
 import android.view.MenuItem
-import com.example.bestsurfspots.databinding.ActivityMainBinding
+
+import com.example.bestsurfspots.fragments.HomePageFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomePageFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
         }
-    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -50,9 +37,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
+    //override fun onSupportNavigateUp(): Boolean {
+      //  val navController = findNavController(R.id.nav_host_fragment_content_main)
+       // return navController.navigateUp(appBarConfiguration)
+          //      || super.onSupportNavigateUp()
+   // }
 }
+
